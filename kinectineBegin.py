447b1409b7f9084c09ba98622f700b963490966b
John G Thevos <admin@mindcette.com>
2020-02-28 09:29:20 -0500

    cleaned up kinectineBegin

--- kinectineBegin.py
+++ kinectineBegin.py
@@ -1,18 +1,10 @@
 # kuatroBegin.py
 # 
-# This file starts all the components of the Kuatro.  
-# To start up the Kuatro:
-#     1. Open a Terminal
-#     2. cd to the Kuatro directory.  eg.  cd Dropbox/kuatro
-#     3. Issue the following command:  sh jython.sh kuatroBegin.py
-
+# This file starts all the components of the VisualVeil
 
 from kinectineServer import * 
 from kinectine import *
 
-#####  Start the Server
-server = KuatroServer(verbose = 0)
+server = KuatroServer(verbose = 0)   # start server
 
-##### Start the View
-view = Kinectine()
-
+view = Kinectine()   # start view
