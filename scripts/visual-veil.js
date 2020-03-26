b16cfc2bb8951054acf5c6324bc084c8d3bdd51d
John G Thevos <admin@mindcette.com>
2020-03-26 15:13:15 -0400

    Converted the particle function to class notation for readability.

--- scripts/visual-veil.js
+++ scripts/visual-veil.js
@@ -21,19 +21,21 @@ var shaderTexture;
 // empty list to hold each user's particle systems
 let particleSystems = [];
 
-// actual particle class
-function Particle(x, y, vx, vy) {
-	this.pos = new p5.Vector(x, y);
-	this.vel = new p5.Vector(vx, vy);
-	this.vel.mult(random(10));
-	this.vel.rotate(radians(random(-25, 25)));
-	this.mass = random(1, 20);
-	this.airDrag = random(0.92, 0.98);
-	this.colorIndex = int(random(colorScheme.length));
 
-	this.move = function() {
-		this.vel.mult(this.airDrag);
-		this.pos.add(this.vel);
+class Particle {
+	constructor(x, y, vx, vy) {
+		this.pos = new p5.Vector(x, y);
+		this.vel = new p5.Vector(vx, vy);
+		this.vel.mult(random(10));
+		this.vel.rotate(radians(random(-25, 25)));
+		this.mass = random(1, 20);
+		this.airDrag = random(0.92, 0.98);
+		this.colorIndex = int(random(colorScheme.length));
+
+		this.move = function() {
+			this.vel.mult(this.airDrag);
+			this.pos.add(this.vel);
+		}
 	}
 }
 
