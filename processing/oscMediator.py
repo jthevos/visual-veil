68c6f0250d6ec81886bd8057f6630e14d63dd743
John G Thevos <admin@mindcette.com>
2020-03-26 20:13:50 -0400

    processing component is now CLI compatible.

---
+++ processing/oscMediator.py
@@ -0,0 +1,33 @@
+import random
+from math import *
+
+add_library('oscP5')
+import netP5
+import oscP5
+
+
+osc = None
+remote = None
+
+
+def setup():
+   global osc, remote
+
+   size(700,700)
+   osc = oscP5.OscP5(this, 57111)
+   remote = netP5.NetAddress("127.0.0.1",13000)
+
+def draw():
+    global osc, remote
+    background(0)
+    fill(255)
+    ellipse(mouseX, mouseY, 100, 100)
+    fill(0)
+    text("test processing pure", mouseX - 40, mouseY)
+
+    message = oscP5.OscMessage("/test")
+    message.add(mouseX)
+    message.add(mouseY)
+    print message
+    print osc
+    osc.send(message, remote)
